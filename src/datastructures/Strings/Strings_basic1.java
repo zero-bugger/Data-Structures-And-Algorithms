f0a8cc6ea5ea0f58fad5e74127b58ecec0a7a68e
Krunal Kamble <kamkru01@CSGICORP.COM>
2021-05-04 16:28:12 +0530

    Data-Structures and Algorithms

---
+++ src/datastructures/Strings/Strings_basic1.java
@@ -0,0 +1,22 @@
+package datastructures.Strings;
+
+import java.util.Scanner;
+
+public class Strings_basic1 {
+	
+	static void printString() {
+	
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+		System.out.println("Input String is "+s);
+				
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printString();
+	}
+
+}
