ca20f898349c299c78f2c95fd96b76d59b7e9ccb
Krunal Kamble <kamkru01@CSGICORP.COM>
2021-05-21 10:36:46 +0530

    Data-Structures and Algorithms

---
+++ src/datastructures/Strings/String_Intermediate17.java
@@ -0,0 +1,77 @@
+package datastructures.Strings;
+
+import java.util.Stack;
+
+public class String_Intermediate17 {
+
+	static void prefixToInfix(String s) {
+		
+		
+		Stack<String> st = new Stack<>();
+		
+		for(int i = s.length()-1 ; i >=0 ;i--) {
+			
+			if(s.charAt(i) == '*' || s.charAt(i) == '+' || s.charAt(i) == '/' || s.charAt(i) == '%' 
+					|| s.charAt(i) == '^') {
+				String operand1 = st.pop();
+				String operand2 = st.pop();
+				
+				String expression = "(" + operand1 + s.charAt(i) + operand2 +")";
+				
+				st.push(expression);
+				
+				
+			}
+			
+			else {
+
+				st.push(s.charAt(i)+"");
+			}
+			
+		}
+		
+		
+		
+		System.out.println(st.pop());
+		
+	}
+	static void postfixToInfix(String s) {
+			
+			
+			Stack<String> st = new Stack<>();
+			
+			for(int i = 0 ; i <s.length() ;i++) {
+				
+				if(s.charAt(i) == '*' || s.charAt(i) == '+' || s.charAt(i) == '/' || s.charAt(i) == '%' 
+						|| s.charAt(i) == '^') {
+					String operand1 = st.pop();
+					String operand2 = st.pop();
+					
+					String expression = "(" + operand2 + s.charAt(i) + operand1 +")";
+					st.push(expression);
+					
+					
+				}
+				
+				else {
+	
+					st.push(s.charAt(i)+"");
+				}
+				
+			}
+			
+    		System.out.println(st.pop());
+			
+		}
+	
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//PostFix start loop from left to right and expression is B operator A
+		//PreFix start loop from right to left and expression is A operator B
+		prefixToInfix("*+ab/ef");
+		postfixToInfix("abef*+");
+		
+	}
+
+}
---
+++ src/datastructures/Strings/Strings_Intermediate16.java
@@ -0,0 +1,62 @@
+package datastructures.Strings;
+
+import java.util.Arrays;
+
+public class Strings_Intermediate16 {
+	
+	static void nextGreatestNumber(int s[]) {
+		int low = 0;
+		int index = 0;
+		int swap = 0 ;
+		int firstsmall= 0;
+		
+
+		for(int i = s.length-1;i>0;i--) {
+			low=i;
+			if(s[i-1] < s[i] ) {
+				firstsmall = s[i-1];
+				index = i-1;
+				break;
+			}
+			
+		}
+		if(low == 1) {
+			System.out.println("All elements are in descending order so not possible");
+			return;
+		}
+		
+		for(int i = index+1 ; i<s.length;i++) {
+			if(s[i] > firstsmall) {
+				swap = i;
+			}
+			else if (s[i] < firstsmall) {
+				if(swap > firstsmall) {
+					swapelement(s,swap,index);
+					break;
+				}
+			}
+			
+		}
+		swapelement(s,swap,index);
+		
+	    Arrays.sort(s,index+1,s.length);
+	    for(int i:s) {
+			System.out.print(" "+i);
+		}
+
+		
+	}
+	
+	static void swapelement(int s[],int a,int b) {
+			int temp = s[a];
+			s[a]=s[b];
+			s[b]=temp;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = {2,1,8,7,6,5};
+  		nextGreatestNumber(a);
+	}
+
+}
