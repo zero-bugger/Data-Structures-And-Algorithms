ee85557932d8eb78862bb648e0199c8c5bb4fa96
_krunal <61634643+zero-bugger@users.noreply.github.com>
2021-05-26 10:16:14 +0530

    Add files via upload

---
+++ src/datastructures/Strings/Strings_Intermediate24.java
@@ -0,0 +1,37 @@
+package datastructures.Strings;
+//Print first repeated word in string
+import java.util.*;
+
+public class String_Intermediate24 {
+
+        static void repeatedWord(String s[]) {
+
+                HashMap<String,Integer> mp = new HashMap<>();
+
+                for(int i=0;i<s.length;i++) {
+                        if(mp.containsKey(s[i])) {
+                                mp.put(s[i],mp.get(s[i])+1);
+                        }
+                        else {
+                                mp.put(s[i], 1);
+                        }
+                }
+
+                Set<Map.Entry<String, Integer>> set = mp.entrySet();
+                Iterator<Map.Entry<String, Integer>> it = set.iterator();
+                while(it.hasNext()) {
+                        Map.Entry<String, Integer> v = it.next();
+                        if(v.getValue() == 2) {
+                                System.out.println(v.getKey());
+                                break;
+                        }
+                }
+        }
+
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+                String arr[] = {"aaa", "bbb", "ccc", "bbb", "aaa", "aaa"};
+                repeatedWord(arr);
+        }
+
+}
