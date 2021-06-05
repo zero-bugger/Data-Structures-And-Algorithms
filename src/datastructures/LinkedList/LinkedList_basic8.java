2381de43507961c21c91edd5626b17b1f3193dee
Krunal Kamble <kamkru01@CSGICORP.COM>
2021-06-05 12:23:49 +0530

    Data-Structures and Algorithms

--- src/datastructures/LinkedList/LinkedList_basic8.java
+++ src/datastructures/LinkedList/LinkedList_basic8.java
@@ -4,46 +4,52 @@ public class LinkedList_basic8 {
 	
 	Node head;
 	static class Node{
+		int data;
 		Node next;
 		Node prev;
-		int data;
+		
+		
 		Node(int d){
-			data =d;
+			data=d;
+			
 		}
 	}
 	
-	public void insert(int d) {
+	
+	public void insert(int d)
+    {
+        Node n = new Node(d);
+        n.prev=null;
+        n.next = head;
+        
+        if(head!=null) {
+        	head.prev = n;
+        }
+        head = n;
+    }
+	public void addAtBegin(int d) {
 		Node n = new Node(d);
-		Node temp = head;
-		n.next = null;
-		n.prev = null;
-		if(head == null) {
-			head = n;
-		}
-		else {
-			temp.next = n;
-			n.prev = temp;
-			temp = n;
-		}
 		
+		n.prev = null;
+		n.next = head;
+		
+		head = n;
 	}
+
 	public void show() {
 		Node n = head;
 		while(n!=null) {
 			System.out.print(n.data+"->");
 			n=n.next;
 		}
-		
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList_basic8 list = new LinkedList_basic8();
+		LinkedList_basic8  list = new LinkedList_basic8();
 		list.insert(12);
-		list.insert(23);
-		list.insert(34);
-		list.insert(56);
+		list.insert(32);
+		list.insert(43);
+		list.insert(122);
+		list.addAtBegin(34);
 		list.show();
-
 	}
-
 }
